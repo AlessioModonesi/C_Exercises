35406e007cd0179f7ce697925e7f702a9f7beabb
Alessio Modonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-02-01 17:05:29 +0100

    update exec.c

--- C/Pipe/exec/exec.c
+++ C/Pipe/exec/exec.c
@@ -1,3 +1,4 @@
+//Alessio Modonesi 4F
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,36 +6,51 @@
 
 int main()
 {
-    int status;
+    int piped[2], status;
+    pipe(piped);
     char fileName[] = "file.txt";
-    int fd[2];
-    pipe(fd);
 
     int PID1 = fork();
-    if (PID1 == 0) 
+    if (PID1 < 0)
     {
-        close(1);
-        dup(fd[1]);
-        close(fd[1]);
+        printf("Errore nella generazione del figlio: %d\n", getpid());
+        exit(1);
+    }
+    else if (PID1 == 0) 
+    {
+        close(1); //chiudo lo standard output di PID1
+        dup(piped[1]); //duplico il canale write della pipe
+        close(piped[1]); //chiudo il canale originale
+        close(piped[0]); //chiudo il canale read della pipe
 
-        execl("/bin/sh", "cat", "file.txt", NULL);
+        execl("/bin/cat", "cat", "file.txt", (char* )0); //chiamo la exec, che esegue cat
         exit(0);
     }
     else
     {
         int PID2 = fork();
-        if (PID2 == 0)
+        if (PID2 < 0)
         {
-            close(0);
-            dup(fd[0]);
-            close(fd[0]);
+            printf("Errore nella generazione del figlio: %d\n", getpid());
+            exit(1);
+        }
+        else if (PID2 == 0)
+        {
+            close(0); //chiudo lo standard input di PID2
+            dup(piped[0]); //duplico il canale read della pipe
+            close(piped[0]); //chiudo il canale originale
+            close(piped[1]); //chiudo il canale write della pipe
 
-            execl("/bin/sh", "wc", NULL);
+            execl("/bin/wc", "wc", (char* )0); //chiamo la exec, che esegue wc
             exit(0);
         }
         else
-            wait(&status);
-            printf("Processo terminato\n");
+        {
+            wait(&status); 
+            close(piped[0]); //chiudo il canale read della pipe
+            close(piped[1]); //chiudo il canale write della pipe
+            exit(0);
+        }    
     }
     return 0;
 }
