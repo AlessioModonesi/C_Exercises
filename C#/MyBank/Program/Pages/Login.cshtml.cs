d50185990380f945945d6f5ce267566ee3b0d0c0
AlessioModonesi <alessio.modonesi@iisviolamarchesini.edu.it>
2022-02-25 09:25:51 +0100

    update MyBank

--- C#/MyBank/Program/Pages/Login.cshtml.cs
+++ C#/MyBank/Program/Pages/Login.cshtml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,8 +33,7 @@ namespace MyBank.Pages
                 else
                     return status = "OFF";
             }
-            else
-                return 0;
+            return 0;
         }
     }
 }
